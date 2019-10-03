9b047cc0711a8773131a37b2f537bd77f29d3ebc
Dunja <vodjevic.dunja@gmail.com>
2019-10-03 15:05:07 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,17 @@
+"""
+print("zdravo svima i sta ima")
+if 2<3:
+    print("ojsa ojsa ojsasa")
+else:
+    print("hvjvu")
+
+def veseljak(ime):
+    print("jedna za majstor "+ime)
+    print("djavolu bi dusu dao za meeeraaak")
+
+veseljak("Ljuba")
+"""
+juzVetar=["sinan","semsa","kemal"]
+for i in juzVetar:
+    print(i)
+    
